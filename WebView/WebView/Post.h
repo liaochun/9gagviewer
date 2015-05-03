f4c93cd4d9f513d746288d5796aab0c1879d5a00
liaochun <chunlu.liao@outlook.com>
2015-05-03 15:11:27 -0400

    Revert 349eb12..7a46f0e
    This rolls back to commit 349eb12b6f5f06e499536d8c3277a3595289901c.

--- WebView/WebView/Post.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface Post : UIViewController
-
-    @property (strong, nonatomic) IBOutlet UIWebView *webView;
-    @property (atomic, strong) NSString *currentURL;
-    @property (atomic, strong) NSString *navigationTitle;
-
-@end
