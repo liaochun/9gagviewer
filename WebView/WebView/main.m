ea005c70cd411c8c36c32f0257d8238daa10c2df
liaochun <chunlu.liao@outlook.com>
2015-05-03 15:13:14 -0400

    init commit

---
+++ WebView/WebView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WebView
+//
+//  Created by Jean-Luc David on 2015-05-03.
+//  Copyright (c) 2015 com.jldavid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
